e129f80224c6413d04a7d2e35a12a273c5524983
ram <ram_r@live.com>
2025-04-23 14:28:39 +0400

    BASE

---
+++ src/main/java/com/cv/s3003unitservicemigration/S3003UnitServiceMigration.java
@@ -0,0 +1,13 @@
+package com.cv.s3003unitservicemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3003UnitServiceMigration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3003UnitServiceMigration.class, args);
+    }
+
+}
